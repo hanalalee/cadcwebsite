7f1104f043af66459d3f2f28fcc50ffd3f879e69
hanalalee <hanalalee103@gmail.com>
2020-09-16 01:23:11 -0700

    automated slideshow

--- index.js
+++ index.js
@@ -1,35 +1,27 @@
 const slider = document.querySelector('.home-slider');
 
-const leftArrow = document.querySelector('.left');
-const rightArrow = document.querySelector('.right');
 const indicatorParents = document.querySelector('.home-controls ul');
 var sectionIndex = 0;
 
 function setIndex(){
     document.querySelector('.home-controls .selected').classList.remove('selected');
-    slider.style.transform = 'translate(' + (sectionIndex) * -25 + '%)';
-}
+    slider.style.transform = 'translate(' + (sectionIndex) * -25 + '%)'};
 
 document.querySelectorAll('.home-controls li').forEach(function(indicator, ind){
     indicator.addEventListener('click', function(){
         sectionIndex = ind; 
-        setIndex(sectionIndex);
+        setIndex();
         indicator.classList.add('selected');
-        
     });
 });
 
-
-leftArrow.addEventListener('click', function() {
-    sectionIndex = (sectionIndex > 0) ? sectionIndex - 1: 0;
-    setIndex();
-    indicatorParents.children[sectionIndex].classList.add('selected');
-    
-});
-
-rightArrow.addEventListener('click', function() {
+setInterval(function(){
     sectionIndex = (sectionIndex < 3) ? sectionIndex + 1: 3;
     setIndex();
     indicatorParents.children[sectionIndex].classList.add('selected');
-    
-});
+    if (sectionIndex == 3){
+        sectionIndex = -1;
+    }
+}, 5000);
+
+
